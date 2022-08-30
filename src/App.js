707d09b53760f7df92e3dd3794a42c53f933baef
ShubhamPrakash <56962038+shubhamprkash@users.noreply.github.com>
2022-08-31 01:26:01 +0530

    val-vid div added + extra margin fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import CinematicVideo from './components/CinematicVideo';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-       <>
+       <div>
         <Navbar/>
         <Footer/>
-       </> 
+        <CinematicVideo/>
+       </div> 
     </div>
   );
 }
